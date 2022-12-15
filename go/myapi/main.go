35777b51e8be9a0897af09745cf48c16cc41ffe1
enokawa <enokawa00@gmail.com>
2022-12-15 22:36:27 +0900

    use gorilla/mux

--- go/myapi/main.go
+++ go/myapi/main.go
@@ -5,16 +5,19 @@ import (
 	"net/http"
 
 	"github.com/enokawa/myapi/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ArticleListHandler)
-	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
