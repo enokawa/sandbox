58dc8b64c7d7a1dc014c71b7c26bcffeaf30e3e5
enokawa <enokawa00@gmail.com>
2023-01-04 22:34:54 +0900

    fix

--- go/myapi/repositories/main_test.go
+++ go/myapi/repositories/main_test.go
@@ -19,22 +19,6 @@ var (
 	dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 )
 
-func setup() error {
-	if err := connectDB(); err != nil {
-		return err
-	}
-	if err := cleanupDB(); err != nil {
-		fmt.Println("cleanup", err)
-		return err
-	}
-	if err := setupTestData(); err != nil {
-		fmt.Println("setup", err)
-		return err
-	}
-
-	return nil
-}
-
 func connectDB() error {
 	var err error
 	testDB, err = sql.Open("mysql", dbConn)
@@ -59,6 +43,22 @@ func cleanupDB() error {
 	cmd := exec.Command("mysql", "-h", "127.0.0.1", "-udocker", "-Dsampledb", "-pdocker", "-e", "source ./testdata/cleanupDB.sql")
 	err := cmd.Run()
 	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setup() error {
+	if err := connectDB(); err != nil {
+		return err
+	}
+	if err := cleanupDB(); err != nil {
+		fmt.Println("cleanup", err)
+		return err
+	}
+	if err := setupTestData(); err != nil {
+		fmt.Println("setup", err)
 		return err
 	}
 
