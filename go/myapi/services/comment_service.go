1886d6d7a6969479f08dc09eb8afbd5babc1fb51
enokawa <enokawa00@gmail.com>
2023-01-04 23:12:49 +0900

    Add PostCommentService

---
+++ go/myapi/services/comment_service.go
@@ -0,0 +1,20 @@
+package services
+
+import (
+	"github.com/enokawa/sandbox/go/myapi/models"
+	"github.com/enokawa/sandbox/go/myapi/repositories"
+)
+
+func PostCommentService(comment models.Comment) (models.Comment, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Comment{}, err
+	}
+
+	newComment, err := repositories.InsertComment(db, comment)
+	if err != nil {
+		return models.Comment{}, nil
+	}
+
+	return newComment, nil
+}
