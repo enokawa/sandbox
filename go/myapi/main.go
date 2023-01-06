71da6725d96cbe9fa122ae0b47fdf8f5d50f67a3
enokawa <enokawa00@gmail.com>
2023-01-06 22:39:43 +0900

    Add router

--- go/myapi/main.go
+++ go/myapi/main.go
@@ -10,8 +10,8 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/enokawa/sandbox/go/myapi/controllers"
+	"github.com/enokawa/sandbox/go/myapi/routers"
 	"github.com/enokawa/sandbox/go/myapi/services"
-	"github.com/gorilla/mux"
 )
 
 var (
@@ -36,15 +36,10 @@ func main() {
 		log.Println("fail to connect DB")
 		return
 	}
+
 	ser := services.NewMyAppService(db)
 	con := controllers.NewMyAppController(ser)
-	r := mux.NewRouter()
-
-	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
+	r := routers.NewRouter(con)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
