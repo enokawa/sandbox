980c64851787abb06e07ad0cf36c04b3f8a1aa22
enokawa <enokawa00@gmail.com>
2023-01-06 22:36:30 +0900

    Connect DB in main

--- go/myapi/main.go
+++ go/myapi/main.go
@@ -1,24 +1,50 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 
-	"github.com/enokawa/sandbox/go/myapi/handlers"
+	"github.com/enokawa/sandbox/go/myapi/controllers"
+	"github.com/enokawa/sandbox/go/myapi/services"
 	"github.com/gorilla/mux"
 )
 
+var (
+	dbUser     = os.Getenv("USERNAME")
+	dbPassword = os.Getenv("USERPASS")
+	dbDatabase = os.Getenv("DATABASE")
+	dbConn     = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+)
+
+func connectDB() (*sql.DB, error) {
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
 func main() {
+	db, err := connectDB()
+	if err != nil {
+		log.Println("fail to connect DB")
+		return
+	}
+	ser := services.NewMyAppService(db)
+	con := controllers.NewMyAppController(ser)
 	r := mux.NewRouter()
 
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
