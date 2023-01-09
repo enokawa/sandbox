9da353f406383d2aea12dba7236b79204f51c829
enokawa <enokawa00@gmail.com>
2023-01-09 21:34:54 +0900

    Add Error

--- go/myapi/services/comment_service.go
+++ go/myapi/services/comment_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/enokawa/sandbox/go/myapi/apperrors"
 	"github.com/enokawa/sandbox/go/myapi/models"
 	"github.com/enokawa/sandbox/go/myapi/repositories"
 )
@@ -8,7 +9,8 @@ import (
 func (s *MyAppService) PostCommentService(comment models.Comment) (models.Comment, error) {
 	newComment, err := repositories.InsertComment(s.db, comment)
 	if err != nil {
-		return models.Comment{}, nil
+		err = apperrors.InsertDataFailed.Wrap(err, "fail to record data")
+		return models.Comment{}, err
 	}
 
 	return newComment, nil
